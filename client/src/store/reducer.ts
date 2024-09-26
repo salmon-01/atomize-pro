b0d0ad1517ca703cf0df781c854b84a85d0b0bcc
Josh <joshuaisaact@gmail.com>
2024-09-26 14:50:22 +0100

    feat: initial useReducer

--- client/src/store/reducer.ts
+++ client/src/store/reducer.ts
@@ -1,26 +1,49 @@
+// IMPORT TAB, GOAL AS TYPES
 
-
-export Type Action =
-| { type: 'SET_TABS'; payload: Tab[] }
-| { type: 'SET_GOALS'; payload: Goals[] }
-| { type: 'CREATE_TAB'; payload: Tab }
-| { type: 'CREATE_GOAL'; payload: Goal }
-| { type: 'UPDATE_GOAL'; payload: UNSURE_YET }
-| { type: 'UPDATE_LIST'; payload: UNSURE_YET }
-| { type: 'DELETE_TAB'; payload: UNSURE_YET };
-| { type: 'DELETE_GOAL'; payload: UNSURE_YET };
-| { type: 'DELETE_LIST_POSITION'; payload: UNSURE_YET };
-| { type: 'SET_LOADING'; payload: boolean };
+export type Action =
+  | { type: "SET_TABS"; payload: Tab[] }
+  | { type: "SET_GOALS"; payload: Goal[] }
+  | { type: "CREATE_TAB"; payload: Tab }
+  | { type: "CREATE_GOAL"; payload: Goal }
+  // | { type: 'UPDATE_GOAL'; payload: UNSURE_YET }
+  // | { type: 'UPDATE_LIST'; payload: UNSURE_YET }
+  // | { type: 'DELETE_TAB'; payload: UNSURE_YET };
+  // | { type: 'DELETE_GOAL'; payload: UNSURE_YET };
+  // | { type: 'DELETE_LIST_POSITION'; payload: UNSURE_YET };
+  | { type: "SET_LOADING"; payload: boolean };
 
 export const initialState = {
-  tabs: [] as Destination[],
-  goals: [] as Triptype[],
+  tabs: [] as Tab[],
+  goals: [] as Goal[],
   isLoading: false,
 };
 
-export function reducer(state: typeof initialState = initialState, action: Action) {
+export function reducer(
+  state: typeof initialState = initialState,
+  action: Action
+) {
   switch (action.type) {
-    case 'SET_LOADING':
+    case "SET_TABS":
+      return {
+        ...state,
+        tabs: action.payload,
+      };
+    case "SET_GOALS":
+      return {
+        ...state,
+        goals: action.payload,
+      };
+    case "CREATE_TAB":
+      return {
+        ...state,
+        tabs: [...state.tabs, action.payload],
+      };
+    case "CREATE_GOAL":
+      return {
+        ...state,
+        tabs: [...state.goals, action.payload],
+      };
+    case "SET_LOADING":
       return {
         ...state,
         isLoading: action.payload,
