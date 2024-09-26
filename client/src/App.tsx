34b233444fde550b7604cfc065a25bb099aa12a9
Kier <kieransmozer@gmail.com>
2024-09-26 14:50:52 +0100

    Cleaning up Navbar file structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react'
+import { React, useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar.jsx';
 import CreateNew from './components/CreateNew.jsx';
@@ -8,11 +8,10 @@ import CreateNewGoal from './components/CreateNewGoal.jsx';
 import MakeEdits from './components/MakeEdits.jsx';
 import HomePage from './components/HomePage.jsx';
 import Tab from './components/Tab.jsx';
-import { fetchAllTabs, fetchAllGoals } from "./ApiService.jsx";
-import './App.css'
+import { fetchAllTabs, fetchAllGoals } from './ApiService.jsx';
+import './App.css';
 
 function App() {
-
   const [tabs, setTabs] = useState([]);
   const [goals, setGoals] = useState([]);
   // const [lists, setLists] = useState([]);
@@ -21,17 +20,23 @@ function App() {
   const [currentXP, setCurrentXP] = useState(0);
 
   const calculateXPGoal = (goals) => {
-    goals.forEach(goal => {
-      goal.type === 'Simple List' && setGoalXPBar(prev => prev + 1);
-      goal.type === 'Levels' && setGoalXPBar(prev => prev + 3);
-      goal.type === 'Sets' && setGoalXPBar(prev => prev + goal.sets);
-      goal.type === 'Progress Bar' && setGoalXPBar(prev => prev + 10);
+    goals.forEach((goal) => {
+      goal.type === 'Simple List' && setGoalXPBar((prev) => prev + 1);
+      goal.type === 'Levels' && setGoalXPBar((prev) => prev + 3);
+      goal.type === 'Sets' && setGoalXPBar((prev) => prev + goal.sets);
+      goal.type === 'Progress Bar' && setGoalXPBar((prev) => prev + 10);
     });
-    goals.forEach(goal => {
-      goal.type === 'Simple List' && goal.complete && setCurrentXP(prev => prev + 1);
-      goal.type === 'Sets' && setCurrentXP(prev => prev + goal.completed_sets);
-      goal.type === 'Levels' && setCurrentXP(prev => prev + goal.level);
-      goal.type === 'Progress Bar' && setCurrentXP(prev => prev + Math.round(goal.current / goal.goal_number * 10));
+    goals.forEach((goal) => {
+      goal.type === 'Simple List' &&
+        goal.complete &&
+        setCurrentXP((prev) => prev + 1);
+      goal.type === 'Sets' &&
+        setCurrentXP((prev) => prev + goal.completed_sets);
+      goal.type === 'Levels' && setCurrentXP((prev) => prev + goal.level);
+      goal.type === 'Progress Bar' &&
+        setCurrentXP(
+          (prev) => prev + Math.round((goal.current / goal.goal_number) * 10)
+        );
     });
   };
 
@@ -39,67 +44,89 @@ function App() {
     try {
       const fetchedTabs = await fetchAllTabs();
       if (fetchedTabs && fetchedTabs.length > 0) {
-          setTabs(fetchedTabs);
+        setTabs(fetchedTabs);
       } else {
-          setTabs([]);
+        setTabs([]);
       }
-  } catch (error) {
-      console.error("Error fetching tabs:", error);
+    } catch (error) {
+      console.error('Error fetching tabs:', error);
       setTabs([]);
     }
   };
 
   const loadGoals = async () => {
     try {
-        const fetchedGoals = await fetchAllGoals();
-        if (fetchedGoals && Array.isArray(fetchedGoals.simple) && Array.isArray(fetchedGoals.progbars) && Array.isArray(fetchedGoals.levels) && Array.isArray(fetchedGoals.sets)) {
-            const allGoals = [
-                ...fetchedGoals.simple,
-                ...fetchedGoals.progbars,
-                ...fetchedGoals.levels,
-                ...fetchedGoals.sets
-            ];
-            setGoals(allGoals);
-        } else {
-            setGoals([]);
-        }
-    } catch (error) {
-        console.error("Error fetching goals:", error);
+      const fetchedGoals = await fetchAllGoals();
+      if (
+        fetchedGoals &&
+        Array.isArray(fetchedGoals.simple) &&
+        Array.isArray(fetchedGoals.progbars) &&
+        Array.isArray(fetchedGoals.levels) &&
+        Array.isArray(fetchedGoals.sets)
+      ) {
+        const allGoals = [
+          ...fetchedGoals.simple,
+          ...fetchedGoals.progbars,
+          ...fetchedGoals.levels,
+          ...fetchedGoals.sets,
+        ];
+        setGoals(allGoals);
+      } else {
         setGoals([]);
-    }
-};
-
-    useEffect(() => {
-      loadTabs();
-      loadGoals();
-    }, []);
-
-    useEffect(() => {
-      if (goals.length > 0) {
-        calculateXPGoal(goals);
       }
-    }, [goals]);
+    } catch (error) {
+      console.error('Error fetching goals:', error);
+      setGoals([]);
+    }
+  };
+
+  useEffect(() => {
+    loadTabs();
+    loadGoals();
+  }, []);
+
+  useEffect(() => {
+    if (goals.length > 0) {
+      calculateXPGoal(goals);
+    }
+  }, [goals]);
 
   return (
     <div className="wrapper">
-      <NavBar goalXPBar={goalXPBar} currentXP={currentXP} tabs={tabs}/>
+      <NavBar goalXPBar={goalXPBar} currentXP={currentXP} tabs={tabs} />
       <Routes>
-        <Route path="/home/*" element={<HomePage tabs={tabs} goals={goals} />} />
+        <Route
+          path="/home/*"
+          element={<HomePage tabs={tabs} goals={goals} />}
+        />
         <Route path="/create-new" element={<CreateNew />} />
-        <Route path="/create-new/list" element={<CreateNewList loadGoals={loadGoals} tabs={tabs} />} />
-        <Route path="/create-new/tab" element={<CreateNewTab tabs={tabs}/>} />
-        <Route path="/create-new/goal" element={<CreateNewGoal tabs={tabs}/>} />
-        <Route path="/edit" element={<MakeEdits tabs={tabs} goals={goals}/>} />
-        {tabs.length > 0 && tabs.map(tab => {
-          if (tab.name) {
-            const hyphenatedName = tab.name.replace(/\s+/g, '-');
-            return <Route key={hyphenatedName} path={`/${hyphenatedName}`} element={<Tab tab={tab} goals={goals} />} />;
-        }
-          return null;
-        })}
+        <Route
+          path="/create-new/list"
+          element={<CreateNewList loadGoals={loadGoals} tabs={tabs} />}
+        />
+        <Route path="/create-new/tab" element={<CreateNewTab tabs={tabs} />} />
+        <Route
+          path="/create-new/goal"
+          element={<CreateNewGoal tabs={tabs} />}
+        />
+        <Route path="/edit" element={<MakeEdits tabs={tabs} goals={goals} />} />
+        {tabs.length > 0 &&
+          tabs.map((tab) => {
+            if (tab.name) {
+              const hyphenatedName = tab.name.replace(/\s+/g, '-');
+              return (
+                <Route
+                  key={hyphenatedName}
+                  path={`/${hyphenatedName}`}
+                  element={<Tab tab={tab} goals={goals} />}
+                />
+              );
+            }
+            return null;
+          })}
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import {React, useState, useEffect} from "react";
+import { React, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import HomeButton from '../assets/navigation/home-button.png';
 import CreateButton from '../assets/navigation/createnew-button.png';
@@ -7,53 +7,64 @@ import EditButton from '../assets/navigation/edit-button.png';
 import '../styles/ProgressBar.css';
 import '../styles/NavBar.css';
 
-export default function NavBar({tabs, goalXPBar, currentXP}) {
-
-    // const [hoveredText, setHoveredText] = ('');
-
-    const formattedDate = formatDate(new Date())
-
-    return (
-        <>
-        <div className="nav-container">
-            <div className="home">
-                <Link to="/home"><img src={HomeButton} className="nav-icon"/></Link>
-            </div>
-            <div className='categories'>
-                {tabs.length > 0 && tabs.map(tab => {
-                    if (tab.name) {
-                        const hyphenatedName = tab.name.replace(/\s+/g, '-');
-                        return <Link to={`/${hyphenatedName}`} key={tab.name}><img src={tab.icon} className="nav-icon"/></Link>;
-                    }
-                    return null;
-                })}
-            </div>
-            <div className={`${tabs.length ? 'categories' : null}`}>
-                {/* <img src={PlanetButton} className="nav-icon" /> */}
-                <Link to="/create-new"><img src={CreateButton} className="nav-icon" /></Link>
-                <Link to="/edit"><img src={EditButton} className="nav-icon" /></Link>
-            </div>
-            <div className="date-display">{formattedDate}</div>
-            <div className="barBox" id="nav-completion-bar-box">
-                <div className="progress-container" id="nav-completion-bar">
-                    <div className='progress-bar xpbar-fill' style={{ width: `${(currentXP / goalXPBar) * 100}%` }}></div>
-                </div>
-            </div>
-        </div>
-        {/* <span className="hover-text">{hoveredText}</span> */}
-        </>
-    )
-}
-
-const formatDate = (date) => {
+export default function NavBar({ tabs, goalXPBar, currentXP }) {
+  const formatDate = (date) => {
     const dateFormat = date.toLocaleDateString('en-GB', {
-        weekday: 'long',
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric'
-      });
+      weekday: 'long',
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    });
     const parts = dateFormat.split(' ');
     const dayOfWeek = parts[0];
     const restOfDate = parts.slice(1).join(' ');
     return `${dayOfWeek}, ${restOfDate}`;
+  };
+
+  const formattedDate = formatDate(new Date());
+
+  return (
+    <>
+      <div className="nav-container">
+        <div className="home">
+          <Link to="/home">
+            <img src={HomeButton} className="nav-icon" />
+          </Link>
+        </div>
+        <div className="categories">
+          {tabs.length > 0 &&
+            tabs.map((tab) => {
+              if (tab.name) {
+                const hyphenatedName = tab.name.replace(/\s+/g, '-');
+                return (
+                  <Link to={`/${hyphenatedName}`} key={tab.name}>
+                    <img src={tab.icon} className="nav-icon" />
+                  </Link>
+                );
+              }
+              return null;
+            })}
+        </div>
+        <div className={`${tabs.length ? 'categories' : null}`}>
+          {/* <img src={PlanetButton} className="nav-icon" /> */}
+          <Link to="/create-new">
+            <img src={CreateButton} className="nav-icon" />
+          </Link>
+          <Link to="/edit">
+            <img src={EditButton} className="nav-icon" />
+          </Link>
+        </div>
+        <div className="date-display">{formattedDate}</div>
+        <div className="barBox" id="nav-completion-bar-box">
+          <div className="progress-container" id="nav-completion-bar">
+            <div
+              className="progress-bar xpbar-fill"
+              style={{ width: `${(currentXP / goalXPBar) * 100}%` }}
+            ></div>
+          </div>
+        </div>
+      </div>
+      {/* <span className="hover-text">{hoveredText}</span> */}
+    </>
+  );
 }
