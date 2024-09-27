7b2bd5ad5501644e21d0d5186b66539b2c9f2cc9
ilyaonop <ilya.onop@gmail.com>
2024-09-27 22:33:42 +0100

    Simple list model defined

---
+++ server/src/models/simpleList.ts
@@ -0,0 +1,47 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "./index";
+import { Tabs } from "./tabs";
+
+class SimpleList extends Model {
+  public id!: number;
+  public list_name!: string;
+  public task_name!: string;
+  public complete!: boolean;
+  public tab!: number;
+  public color?: string;
+}
+
+SimpleList.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    list_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    task_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    complete: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    tableName: "simplelists",
+  }
+);
+
+SimpleList.belongsTo(Tabs, { foreignKey: "tab" });
+Tabs.hasMany(SimpleList, { foreignKey: "tab" });
+
+export { SimpleList };
