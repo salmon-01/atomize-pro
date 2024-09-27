ff4487b15bdfd0b9c1cec02d2f994f17b0d6e88b
Kier <kieransmozer@gmail.com>
2024-09-27 12:33:41 +0100

    TS refactors for AddSimple and CreateNew files, nothing needed to be done for either

---
+++ client/src/components/AddSimple.tsx
@@ -0,0 +1,67 @@
+//! Doesn't need TSX as it's a non-functioning component -- See Sara's message in group chat
+export default function AddSimple() {
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <th> </th>
+            <th>Goal name</th>
+            <th>Color</th>
+          </tr>
+          <tr key={`goal-${index}`}>
+            <td
+              className="remove-by-index"
+              onClick={() => {
+                removeItem(index);
+              }}
+            >
+              <img src={Delete} className="delete-icon" />
+            </td>
+            <td>
+              <input
+                type="text"
+                className={`name-goal name-simple ${goal.color}`}
+                value={goal.name}
+                onChange={(e) => handleGoalNameChange(index, e.target.value)}
+              />
+            </td>
+            <td>
+              <div
+                className={`color-box ${goal.color}`}
+                value={goal.color}
+                onClick={openColorBox}
+              ></div>
+              <div className="color-choices">
+                <div
+                  className="color-option purple"
+                  onClick={() => {
+                    handleGoalColorChange(index, "purple");
+                  }}
+                ></div>
+                <div
+                  className="color-option yellow-green"
+                  onClick={() => {
+                    handleGoalColorChange(index, "yellow-green");
+                  }}
+                ></div>
+                <div
+                  className="color-option orange"
+                  onClick={() => {
+                    handleGoalColorChange(index, "orange");
+                  }}
+                ></div>
+                <div
+                  className="color-option red"
+                  onClick={() => {
+                    handleGoalColorChange(index, "red");
+                  }}
+                ></div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
-import React, { StrictMode } from 'react'
-import ReactDOM, { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.js'
-import './index.css'
+import React, { StrictMode } from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.js";
+import "./index.css";
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
