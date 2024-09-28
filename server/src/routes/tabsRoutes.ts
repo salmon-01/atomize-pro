8f0b3e4eb05bae5434c06b19720dd39564357b42
ilyaonop <ilya.onop@gmail.com>
2024-09-28 14:43:38 +0100

    Tab route added: fetch all lists

--- server/src/routes/tabsRoutes.ts
+++ server/src/routes/tabsRoutes.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { getAllTabs, createTab, deleteTab } from "../controllers/tabController";
+import {
+  getAllTabs,
+  createTab,
+  deleteTab,
+  getAllListsFromTab,
+} from "../controllers/tabController";
 
 const router = express.Router();
 
 router.get("/", getAllTabs);
+router.get("/:tabId/lists", getAllListsFromTab);
 router.post("/", createTab);
 router.delete("/:id", deleteTab);
 
